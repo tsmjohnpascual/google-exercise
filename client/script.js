164f3d9c09152c5b8e57900f582af2bf71a5fada
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-07-20 14:11:13 +0100

    add app.get

---
+++ client/script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', getSearch());
+
+
+;
+
+    function getSearch() {
+        
+    }
--- server/app.js
+++ server/app.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fetch = require('node-fetch');
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-User-Agent': 'desktop',
+		'X-Proxy-Location': 'EU',
+		'X-RapidAPI-Key': '143c94ba97mshe6529c6df88575ep1a5f73jsn63e72cb01971',
+		'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'
+	}
+};
+
+function displayData (text) {
+
+    const display = document.getElementById('text');
+    display.textContent = text;
+
+}
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server listening on ${port}`)
 })
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+app.get ('/search', (req, res) => {
+    fetch('https://google-search3.p.rapidapi.com/api/v1/search/q=elon+musk', options)
+	.then(response => response.json())
+	.then(response => res.json(response))
+	.catch(err => console.error(err));
+    
+});
