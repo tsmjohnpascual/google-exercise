6625bb5060da89820e5b1da8ed535c674e516391
johnpascual <ashleypascual@hotmail.es>
2022-07-20 17:02:44 +0100

    client files

--- client/script.js
+++ client/script.js
@@ -1,10 +1,10 @@
-const form = document.querySelector('form')
+const searchFunctions = require("../server/functions")
+const googleSearchButton = document.getElementById("google_search")
+const feelingLuckyButton = document.getElementById("feeling_lucky")
 
-form.addEventListener('submit', getSearch());
+googleSearchButton.addEventListener("submit", searchFunctions.determineSearch(googleSearchButton.id))
+feelingLuckyButton.addEventListener("submit", searchFunctions.determineSearch(feelingLuckyButton.id))
 
 
-;
 
-    function getSearch() {
-        
-    }
+
